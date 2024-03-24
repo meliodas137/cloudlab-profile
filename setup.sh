014981d41ab86e2a7afcbf8d14e2d3454b631de3
meliodas137 <mayankgt1043@gmail.com>
2024-03-24 17:05:37 -0400

    updated script

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
 git clone https://github.com/Clive2312/duckdb
+echo "Installing Tools...\n"
 sudo apt-get update
 sudo apt-get install -y python3-pip
+sudo apt install cmake
+echo "Done...\nInstalling Dependencies...\n"
 pip install numpy
 pip install pandas
 cd ./duckdb/examples/embedded-c++/gen_db/
-echo "Generating data\n"
+echo "Generating data...\n"
 python genDB.py
 echo "Ready!"
