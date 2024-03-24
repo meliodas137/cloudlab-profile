d28390d12e72cabb51f6a7da2f19d61dedd07517
meliodas137 <mayankgt1043@gmail.com>
2024-03-24 18:19:42 -0400

    updated python profile script

--- profile.py
+++ profile.py
@@ -20,6 +20,7 @@ request = pc.makeRequestRSpec()
 node = request.RawPC("node")
 
 # Install and execute a script that is contained in the repository.
+node.addService(pg.Execute(shell="sh", command="echo \"...Initializing node...\""))
 node.addService(pg.Execute(shell="sh", command="chmod +x /local/repository/setup.sh"))
 node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
 
