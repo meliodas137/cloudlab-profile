840f587dfc8c26513c713fa21fa6054803878196
meliodas137 <mayankgt1043@gmail.com>
2024-03-24 17:18:04 -0400

    updated python profile script

--- profile.py
+++ profile.py
@@ -5,17 +5,22 @@ Wait for the profile instance to start, and then log in to the host via the
 ssh port specified below.
 """
 
+# Import the Portal object.
 import geni.portal as portal
-import geni.rspec.pg as rspec
+# Import the ProtoGENI library.
+import geni.rspec.pg as pg
+
+# Create a portal context.
+pc = portal.Context()
 
 # Create a Request object to start building the RSpec.
-request = portal.context.makeRequestRSpec()
+request = pc.makeRequestRSpec()
  
-# Create a raw PC
+# Add a raw PC to the request.
 node = request.RawPC("node")
 
 # Install and execute a script that is contained in the repository.
-node.addService(rspec.Execute(shell="sh", command="/local/repository/setup.sh"))
+node.addService(pg.Execute(shell="sh", command="/local/repository/setup.sh"))
 
 # Print the RSpec to the enclosing page.
-portal.context.printRequestRSpec(request)
+pc.printRequestRSpec(request)
